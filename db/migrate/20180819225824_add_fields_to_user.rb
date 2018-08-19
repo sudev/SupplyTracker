36b0fda815a2e14c31ce4c792f5f75b8516c6606
Ajith Hussain <csy0013@googlemail.com>
2018-08-20 09:00:22 +1000

    More required fields for users

---
+++ db/migrate/20180819225824_add_fields_to_user.rb
@@ -0,0 +1,11 @@
+class AddFieldsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :role, :string
+    add_column :users, :latitude, :decimal,  {precision: 10, scale: 6}
+    add_column :users, :longitude, :decimal,  {precision: 10, scale: 6}
+    add_column :users, :location, :string
+    add_column :users, :address, :text
+    add_column :users, :phone_number, :string
+    add_column :users, :whatsapp_number, :string
+  end
+end
