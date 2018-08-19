8131daaf09fd5325e9edf25d5fcc8f31bb96d36c
Ajith Hussain <csy0013@googlemail.com>
2018-08-19 22:04:18 +1000

    Customised User login/register

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:sessions, :registrations]
+  as :user do
+    get 'login', to: 'users/sessions#new', as: :new_user_session
+    post 'login', to: 'users/sessions#create', as: :user_session
+    delete 'logout', to: 'users/sessions#destroy', as: :destroy_user_session
+    get 'register', to: 'users/registrations#new', as: :new_user_registration
+    post 'register', to: 'users/registrations#create', as: :user_registration
+  end
 end
